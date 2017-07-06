63fc35aaf51ec7e85e26c0f2b337e3fffeb6f438
Cameron Zaas <cameronzaas@gmail.com>
2017-07-06 14:58:59 -0700

    Figured out routing

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -10,6 +10,47 @@ import {
   app,
   h,
 } from 'hyperapp';
+import { Router } from 'hyperapp-router';
+
+const Nav = ({ state, actions }) => {
+  function linkClicked (e) {
+    e.preventDefault();
+    let path = this.attributes.path.value;
+
+    if (path) {
+      actions.router.go(path);
+    }
+  };
+
+  return (
+    <nav>
+      <a onclick={linkClicked} path="/">Home</a> <a onclick={linkClicked} path="/about">About</a> <a onclick={linkClicked} path="/contact">Contact</a>
+    </nav>
+  );
+};
+
+const Home = (state, actions) => {
+  return (
+    <main>
+      <Nav state={state} actions={actions} />
+      <h1>Home</h1>
+    </main>
+  );
+};
+
+const About = (state, actions) => (
+  <main>
+    <Nav state={state} actions={actions} />
+    <h1>About</h1>
+  </main>
+);
+
+const Contact = (state, actions) => (
+  <main>
+    <Nav state={state} actions={actions} />
+    <h1>Contact</h1>
+  </main>
+);
 
 app({
   root: document.getElementById('mount'),
@@ -21,10 +62,12 @@ app({
   actions: {
 
   },
+  
+  mixins: [Router],
 
-  view: (state, actions) => (
-    <main class="container">
-      <h1>Sight Words</h1>
-    </main>
-  ),
+  view: [
+    ['/', Home],
+    ['/about', About],
+    ['/contact', Contact],
+  ],
 });
