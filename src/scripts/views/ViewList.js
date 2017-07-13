c746366e748c7f48d84bb4be822c02f51b9a296e
Cameron Zaas <cameronzaas@gmail.com>
2017-07-12 21:21:34 -0700

    Fixed some punctuation

--- src/scripts/views/ViewList.js
+++ src/scripts/views/ViewList.js
@@ -39,9 +39,9 @@ export const ViewList = (state, actions, data, emit) => {
           This button only shows up on practice list. 
           - It will allow the user to move on to next word without removing current word from practice list so they can practice it later.
         */}
-        <p className={showOnPracticeList}><a onclick={() => actions.leaveOnPracticeAndContinue(currentWord)}>Leave on practice list and continue</a></p>
+        <p className={showOnPracticeList}><a onclick={() => actions.leaveOnPracticeAndContinue(currentWord)}>Leave on practice list and continue.</a></p>
 
-        <p><a onclick={() => actions.goBackOneWord(currentWord)}>Go back one</a></p>
+        <p><a onclick={() => actions.goBackOneWord(currentWord)}>Go back one.</a></p>
       </div>
 
       {/*
